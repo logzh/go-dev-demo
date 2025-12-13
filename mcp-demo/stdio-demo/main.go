057ab4523252ddaa9ff25159c88771a704b5b880
spence <sztoNCNaJF9e72mAjiZERH+spence@noreply.cnb.cool>
2025-12-13 11:35:58 +0800

    feat: update

--- mcp-demo/stdio-demo/main.go
+++ mcp-demo/stdio-demo/main.go
@@ -66,6 +66,8 @@ func main() {
 				return mcp.NewToolResultError("cannot divide by zero"), nil
 			}
 			result = x / y
+		default:
+			return mcp.NewToolResultError("invalid operation"), nil
 		}
 
 		return mcp.NewToolResultText(fmt.Sprintf("%.2f", result)), nil
